b13201db811fc4c09880e77ca1fa6595866a1f7d
KarolWes <karol.wesolowski01@gmail.com>
2023-03-31 15:45:16 +0200

    task 2

--- main.py
+++ main.py
@@ -23,10 +23,37 @@ def task1(filename):
     f.close()
     f_out.close()
 
+def task2(filename):
+    f = open(filename, "r")
+    f_out = open(filename.split('.')[0] + ".out", "w")
+    results = []
+    n, pl = tuple(map(int, f.readline().split(' ')))
+
+
+    for _ in range(n):
+        players = pl
+        styles = list(f.readline().strip())
+        rounds = 2
+        entries = styles
+        results = []
+        for _ in range(rounds):
+            for i in range(0, players, 2):
+                a = min(entries[i], entries[i+1])
+                b = max(entries[i], entries[i+1])
+                results.append(compare(a,b))
+            players = players//2
+            entries = results
+            results = []
+        f_out.write("".join(x for x in entries) + '\n')
+
+
+    f.close()
+    f_out.close()
+
 if __name__ == '__main__':
-    task1("data/level1_example.in")
-    task1("data/level1_1.in")
-    task1("data/level1_2.in")
-    task1("data/level1_3.in")
-    task1("data/level1_4.in")
-    task1("data/level1_5.in")
+    task2("data/level2_example.in")
+    task2("data/level2_1.in")
+    task2("data/level2_2.in")
+    task2("data/level2_3.in")
+    task2("data/level2_4.in")
+    task2("data/level2_5.in")
